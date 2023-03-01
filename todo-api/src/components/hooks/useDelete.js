ecf8d12e0a1f102afffad010c799d6c6c1b75660
CocoNixy <m.nicole1994@gmail.com>
2023-03-01 21:52:20 +0200

    delete nodes

---
+++ todo-api/src/components/hooks/useDelete.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { deleteTodo } from "../../services/deleteTodo";
+
+export const useDelete = (id, onDelete) => {
+    const [open, setOpen] = useState(false);
+
+    const openDeleteDialog = () => {
+        setOpen(true);
+    };
+
+    const closeDeleteDialog = () => {
+        setOpen(false);
+    };
+
+    const handleDelete = async () => {
+        await deleteTodo(id);
+        onDelete?.();
+    };
+
+    return {
+        openDeleteDialog,
+        closeDeleteDialog,
+        handleDelete,
+        isOpen: open,
+    };
+};
---
+++ todo-api/src/components/hooks/useUpdate.js
@@ -0,0 +1,15 @@
+import { updateTodo } from "../../services/updateTodo";
+
+export const useUpdate = ({ id, title, description, completed, onReload }) => {
+
+    const onComplete = async () => {
+        await updateTodo({ _id: id, title, description, completed: true }).then(onReload);
+        onReload();
+    };
+
+    const onIncomplete = async () => {
+        await updateTodo({ _id: id, title, description, completed: false });
+        onReload();
+    };
+    return { onComplete, onIncomplete };
+}
